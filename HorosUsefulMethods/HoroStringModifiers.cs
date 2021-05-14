5b61c122c4278083f3402baeb09f8c4a5aaaa33f
ethan murphy <33498180+H0rologium@users.noreply.github.com>
2021-05-14 14:46:10 -0500

    added intellisense comment

--- HorosUsefulMethods/HoroStringModifiers.cs
+++ HorosUsefulMethods/HoroStringModifiers.cs
@@ -4,6 +4,9 @@ namespace HorosUsefulMethods
 {
     public static class HoroStringModifiers
     {
+		///<summary>
+		///Returns a string with only the leading text of the position of the supplied argument
+		///</summary>
         public static string TextBefore(this String str, string value )
         {
             return str.Substring(0 , str.IndexOf(value)-1);
