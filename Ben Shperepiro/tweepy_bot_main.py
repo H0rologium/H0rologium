568b15fce348d67c18d046d43d18c9df24a29e21
Horologium <33498180+H0rologium@users.noreply.github.com>
2022-07-14 13:01:59 -0500

    Delete Ben Shperepiro directory
    never did anything with this anyways

--- Ben Shperepiro/tweepy_bot_main.py
+++
@@ -1,7 +0,0 @@
-import tweepy
-
-#Auth with the site
-auth = tweepy.OAuthHandler("CONSUMER_KEY", "CONSUMER_SECRET")
-auth.set_access_token("ACCESS_TOKEN", "ACCESS_TOKEN_SECRET")
-#Make API
-api = tweepy.API(auth)
